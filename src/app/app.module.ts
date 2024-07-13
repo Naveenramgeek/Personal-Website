a4e97c63d24f66c3c8bdc002581fae6f08bcddbf
unknown <naveenvemula2487@gmail.com>
2024-07-13 18:04:38 +0530

    adding project

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './home/nav-bar/nav-bar.component';
+import { DisplayImageComponent } from './home/display-image/display-image.component';
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { QuoteComponent } from './home/quote/quote.component';
+import { HomeFooterComponent } from './home/home-footer/home-footer.component';
+import { AboutComponent } from './about/about.component';
+import { TopImageComponent } from './about/top-image/top-image.component';
+import { AboutInfoComponent } from './about/about-info/about-info.component';
+import { AboutSectionComponent } from './about/about-section/about-section.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    DisplayImageComponent,
+    WelcomeComponent,
+    QuoteComponent,
+    HomeFooterComponent,
+    AboutComponent,
+    TopImageComponent,
+    AboutInfoComponent,
+    AboutSectionComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
