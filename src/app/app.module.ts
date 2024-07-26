ad880fb158e33f002bb8bc4b505c9f0924287b76
Naveenramgeek <naveenvemula2487@gmail.com>
2024-07-26 21:57:35 +0530

    Completing the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './home/nav-bar/nav-bar.component';
-import { DisplayImageComponent } from './home/display-image/display-image.component';
-import { WelcomeComponent } from './home/welcome/welcome.component';
-import { QuoteComponent } from './home/quote/quote.component';
 import { HomeFooterComponent } from './home/home-footer/home-footer.component';
 import { AboutComponent } from './about/about.component';
 import { AboutSectionComponent } from './about/about-section/about-section.component';
@@ -15,15 +12,13 @@ import { SkillsComponent } from './skills/skills.component';
 import { TrajectoryComponent } from './trajectory/trajectory.component';
 import { ContactComponent } from './contact/contact.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    DisplayImageComponent,
-    WelcomeComponent,
-    QuoteComponent,
     HomeFooterComponent,
     AboutComponent,
     AboutSectionComponent,
@@ -34,7 +29,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
